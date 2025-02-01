ded80fbac7d08126478185265a06d61f346462af
bmatthews1 <benmatthews0@gmail.com>
2025-02-01 15:58:04 -0800

    added JSON file loading

--- benApp/script.js
+++ benApp/script.js
@@ -1,28 +1,63 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const modelPath = '/data_dump_copy/colored_glb.glb';
+//-- Globals ----------------------------------------------
+    //-- File Path Constants:
+        const dataFolder = '/data_dump_copy';
+        const modelPath = `${dataFolder}/colored_glb.glb`;
+        const jsonFiles = ["adjacency_graph", "adjacency_graph_edge_metadata", "entity_geometry_info", "rgb_id_to_entity_id_map"];
 
-console.log("test");
+    //-- Three.js:
+        const loader = new GLTFLoader();
 
-let gltfMeshes = [];
-const loader = new GLTFLoader();
+    //-- Data Containers:
+        let gltfMeshes = [];
+        let jsonData = {};
 
-let loadModel = () => {
-    loader.load(modelPath, gltf => {
-        gltf.scene.traverse(child => {
-            if (!child.isMesh) return;
+//-- Utils ------------------------------------------------
+    let emptyFn = () => {};
 
-            const mat = child.material;
-            gltfMeshes.push({
-                id : child.id,
-                normals : child.geometry.attributes.normal.array,
-                positions : child.geometry.attributes.position.array,
-                color : ["r", "g", "b"].map(e => child.material.color[e])
+//-- Data Loader Helper Functions -------------------------
+    let modelLoaded = false;
+    let filesLoaded = 0;
+
+    let loadModel = (path) => {
+        loader.load(path, gltf => {
+            gltf.scene.traverse(child => {
+                if (!child.isMesh) return;
+
+                //retrieve geometry, id, and color information
+                gltfMeshes.push({
+                    id : child.id,
+                    normals : child.geometry.attributes.normal.array,
+                    positions : child.geometry.attributes.position.array,
+                    color : ["r", "g", "b"].map(e => child.material.color[e])
+                });
             });
+            console.log(gltfMeshes);
+            modelLoaded = true;
         });
-        console.log(gltfMeshes);
-    });
-}
+    }
 
-loadModel();
+    let loadJSON = (filePath, success=emptyFn, err=emptyFn) => {
+        fetch(filePath)
+        .then(response => response.json())
+        .then(success)
+        .catch(err);
+    }
+
+    let loadData = () => {
+        loadModel(modelPath);
+        for (let file of jsonFiles){
+            loadJSON(`${dataFolder}/${file}.json`, data => {
+                jsonData[file] = data;
+                filesLoaded++;
+            });
+        }
+    }
+
+    let areFilesLoaded = () => {
+        return modelLoaded && filesLoaded == 4;
+    }
+
+loadData();
